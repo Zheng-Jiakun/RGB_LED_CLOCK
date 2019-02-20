5eba962d4ceb41da57ce25007f843cce2005d7b9
Zheng-Jiakun <jzhengaq@ust.hk>
2019-02-20 19:18:07 +0800

    add DS3231 RTC module

--- RGB_LED_CLOCK.ino
+++ RGB_LED_CLOCK.ino
@@ -1,9 +1,15 @@
 #include "FastLED.h"
 #include <SimpleTimer.h>
+#include <DS3231.h>
+// Init the DS3231 using the hardware interface
+DS3231  rtc(SDA, SCL);
+Time ds3231;
+
+unsigned char hh = 0, mm = 0, ss = 0;
 
 SimpleTimer timer;
 
-char second = 0, minute = 0, hour = 0;
+char half_sec_flag = 0;
 
 #define ANIMATION_SPEED  10
 
@@ -413,15 +419,15 @@ void LED_display_num (char b, char d)
 
 void LED_display_dot ()
 {
-  led_buffer[92] = CRGB(LED_color_config[92][0] * (second % 2), LED_color_config[92][1] * (second % 2), LED_color_config[92][2] * (second % 2));
+  led_buffer[92] = CRGB(LED_color_config[92][0] * half_sec_flag, LED_color_config[92][1] * half_sec_flag, LED_color_config[92][2] * half_sec_flag);
   led_buffer[93] = CRGB(LED_color_config[92][0], LED_color_config[92][1], LED_color_config[92][2]);
-  led_buffer[94] = CRGB(LED_color_config[92][0] * !(second % 2), LED_color_config[92][1] * !(second % 2), LED_color_config[92][2] * !(second % 2));
+  led_buffer[94] = CRGB(LED_color_config[92][0] * !half_sec_flag, LED_color_config[92][1] * !half_sec_flag, LED_color_config[92][2] * !half_sec_flag);
 
   led_buffer[95] = CRGB(0, 0, 0);
 
-  led_buffer[96] = CRGB(LED_color_config[96][0] * !(second % 2), LED_color_config[96][1] * !(second % 2), LED_color_config[96][2] * !(second % 2));
+  led_buffer[96] = CRGB(LED_color_config[96][0] * !half_sec_flag, LED_color_config[96][1] * !half_sec_flag, LED_color_config[96][2] * !half_sec_flag);
   led_buffer[97] = CRGB(LED_color_config[97][0], LED_color_config[97][1], LED_color_config[97][2]);
-  led_buffer[98] = CRGB(LED_color_config[98][0] * (second % 2), LED_color_config[98][1] * (second % 2), LED_color_config[98][2] * (second % 2));
+  led_buffer[98] = CRGB(LED_color_config[98][0] * half_sec_flag, LED_color_config[98][1] * half_sec_flag, LED_color_config[98][2] * half_sec_flag);
   FastLED.show();
 }
 
@@ -442,57 +448,90 @@ void LED_display_refresh (char b, char d)
 
 void time_display_refresh()
 {
-  (minute / 10) ?
-  LED_display_refresh(1, minute / 10) :
+  (hh / 10) ?
+  LED_display_refresh(1, hh / 10) :
   LED_diplay_bit_off(1);
 
-  LED_display_refresh(2, minute % 10);
-  LED_display_refresh(3, second / 10);
-  LED_display_refresh(4, second % 10);
-  //  LED_display_refresh(1, hour / 10);
-  //  LED_display_refresh(2, hour % 10);
-  //  LED_display_refresh(3, minute / 10);
-  //  LED_display_refresh(4, minute % 10);
+  LED_display_refresh(2, hh % 10);
+  LED_display_refresh(3, mm / 10);
+  LED_display_refresh(4, mm % 10);
+  LED_display_dot();
 }
 
 void LED_color_refresh()
 {
   for (char i = 0; i < LED_NUM; i ++)
   {
-    LED_color_config[i][0] = 50 * (second % 5 + 1);
-    LED_color_config[i][1] = 60 * (second % 4 + 1);
-    LED_color_config[i][2] = 80 * (second % 3 + 1);
+    LED_color_config[i][0] = 50 * (ss % 5 + 1);
+    LED_color_config[i][1] = 60 * (ss % 4 + 1);
+    LED_color_config[i][2] = 80 * (ss % 3 + 1);
   }
+}
+
+void time_set()
+{
+  if (Serial.available() > 0)
+  {
+    String cmd = Serial.readString();
+    int data = 0;
+    switch (cmd[0])
+    {
+      case 'h':
+        data = (cmd[1] - '0') * 10 + (cmd[2] - '0');
+        rtc.setTime(data, mm, ss);
+        Serial.print("Hour set to ");
+        Serial.println(data);
+        break;
+
+      case 'm':
+        data = (cmd[1] - '0') * 10 + (cmd[2] - '0');
+        rtc.setTime(hh, data, ss);
+        Serial.print("Minute set to ");
+        Serial.println(data);
+        break;
+
+      case 's':
+        data = (cmd[1] - '0') * 10 + (cmd[2] - '0');
+        rtc.setTime(hh, mm, data);
+        Serial.print("Second set to ");
+        Serial.println(data);
+        break;
+    }
+  }
+}
+
+void get_rtc_time ()
+{
+  (ds3231.hour > 12) ?
+  hh = ds3231.hour - 12 :
+       hh = ds3231.hour;
+  mm = ds3231.min;
+  ss = ds3231.sec;
 }
 
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN,  GRB>(led_buffer, LED_NUM);
-  timer.setInterval(1000, repeatMe);
+  timer.setInterval(250, repeatMe);
+  // Initialize the rtc object
+  rtc.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
   timer.run();
-
+  ds3231 = rtc.getTime();
+  get_rtc_time();
   LED_color_refresh();
-
   time_display_refresh();
-  LED_display_dot();
+  time_set();
 }
 
 void repeatMe() {
-  second++;
-  if (second > 59)
+  static char cnt = 0;
+  half_sec_flag = !half_sec_flag;
+  if (cnt++ == 4)
   {
-    second = 0;
-    minute++;
-    if (minute > 59)
-    {
-      minute = 0;
-      hour++;
-      if (hour > 12)
-      {
-        hour = 0;
-      }
-    }
+    cnt = 0;
+    Serial.println("Please enter 'h ,m or s + xx' to set Hour/Min/Sec");
   }
 }
